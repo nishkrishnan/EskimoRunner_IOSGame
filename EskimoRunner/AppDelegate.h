e9371153b9e45d3bc8176bea576b95055b6ed925
Nishad Krishnan <Nishad@Nishad-Krishnans-MacBook-Pro.local>
2014-01-14 19:40:50 -0500

    Initial Commit

---
+++ EskimoRunner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EskimoRunner
+//
+//  Created by Nishad Krishnan on 1/14/2014.
+//  Copyright (c) 2014 TheBusiness. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EskimoRunner/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  EskimoRunner
+//
+
+//  Copyright (c) 2014 TheBusiness. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
